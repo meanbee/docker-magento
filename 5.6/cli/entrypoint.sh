bcaf7dcf32b24205a3ee503d642ea54b629372bc
Nick Jones <nick@nicksays.co.uk>
2016-05-23 13:19:28 +0100

    Don't start cron service in background

--- 5.6/cli/entrypoint.sh
+++ 5.6/cli/entrypoint.sh
@@ -3,10 +3,7 @@
 [ "$DEBUG" = "true" ] && set -x
 
 # Setup Magento cron
-if [ "$ENABLE_CRON" = "true" ]; then
-    echo "* * * * * root su www-data -s /bin/bash -c 'sh $MAGE_ROOT_DIR/cron.sh'" > /etc/cron.d/magento
-    /etc/init.d/cron start
-fi
+echo "* * * * * root su www-data -s /bin/bash -c 'sh $MAGE_ROOT_DIR/cron.sh'" > /etc/cron.d/magento
 
 # Start sendmail
 /etc/init.d/sendmail start
